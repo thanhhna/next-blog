76f5f307eb1b803edd2bda5585f6e7a4d465884f
Nguyen Viet Thanh <thanh@midoriki.com>
2020-05-08 18:57:02 +0900

    Initial commit

---
+++ components/layout.js
@@ -0,0 +1,68 @@
+import styles from './layout.module.css';
+import Head from 'next/head';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Thanh Nguyen';
+export const siteTitle = `Thanh's corner`;
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link ref="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content={`Thanh's personal little corner.`}
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/ava.png" 
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/ava.png" 
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
